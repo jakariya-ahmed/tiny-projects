ef38d56d97a5fc9b192e25821ae0768b3c5590c3
jek9356 <devjakariya9356@gmail.com>
2025-08-08 01:03:38 +0600

    partial projects initial commit

---
+++ react-partial-projects/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+
+export default function Navbar() {
+    return (
+        <header>
+            <div>
+                {/* logo section  */}
+                <img src="" alt="" srcset="" />
+            </div>
+            <div>
+                {/* menu section  */}
+            </div>
+            <div>
+                {/* call to action section  */}
+            </div>
+        </header>
+    );
+}
