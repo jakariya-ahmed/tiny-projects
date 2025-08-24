47fd00d75172fca11e837ecf923b156a36fb646d
jek9356 <devjakariya9356@gmail.com>
2025-08-24 15:58:31 +0600

    initial comit

---
+++ react-core/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='text-violet-500 text-3xl text-center'>This is core React </h2>
+    </>
+  )
+}
+
+export default App
