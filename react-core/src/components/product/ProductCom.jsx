47bbb611365486537b2de7ca89a18f409f80ae7a
jek9356 <devjakariya9356@gmail.com>
2025-09-28 16:34:09 +0600

    create product card design

--- react-core/src/components/product/ProductCom.jsx
+++ react-core/src/components/product/ProductCom.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
 export default function ProductCom() {
+    // State fro products, loading, and error
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    // Fetch products using Fetch Api form dummyjson.com
+    
     return(
-        <section className="">
-            <h2> Products component </h2>
+        <section className="my-6 grid sm:grid-cols-2 md:px-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+            <div className="shadow-md rounded-md">
+                <img src="/images/product2.jpg" alt="skin-product" />
+            <div className="p-4">
+                <h2 className="text-lg">Best skin care crime for women</h2>
+                <p><del>3450tk </del> <strong>300tk</strong></p>
+                <button className="bg-amber-500 text-white px-4 py-1 rounded-md mt-2 hover:bg-amber-600 cursor-pointer">Add to cart</button>
+            </div>
+            </div>
+            
         </section>
     );
 }
--- react-core/src/pages/Products.jsx
+++ react-core/src/pages/Products.jsx
@@ -3,7 +3,7 @@ import ProductCom from "../components/product/ProductCom";
 export default function Products() {
     return(
         <div className="max-w-7xl mx-auto">
-            <h2> Product Page </h2>
+            
             {/* products component */}
             <section>
                 <ProductCom />
