358e2187c9484d943fcd1ef024b5040d9e0b99f8
jek9356 <devjakariya9356@gmail.com>
2025-10-19 13:09:10 +0600

    remove product image hight

--- react-core/src/components/product/ProductCard.jsx
+++ react-core/src/components/product/ProductCard.jsx
@@ -23,7 +23,7 @@ export default function ProductCard({ product }) {
         <img
           src={product.thumbnail}
           alt={product.title}
-          className="w-full object-cover transition-transform duration-300 group-hover:scale-105"
+          className="w-full transition-transform duration-300 group-hover:scale-105"
         />
 
         {/* Product Info */}
