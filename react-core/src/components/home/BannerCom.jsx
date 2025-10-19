804e4c976aa872873f714cf9fcc77a81cde5a4a3
jek9356 <devjakariya9356@gmail.com>
2025-10-19 14:26:30 +0600

    make responsive banner

--- react-core/src/components/home/BannerCom.jsx
+++ react-core/src/components/home/BannerCom.jsx
@@ -2,11 +2,11 @@ import { h2 } from "framer-motion/client";
 
 export default function BannerCom() {
     return (
-        <div className="my-8">
+        <div className="my-4 lg:my-8">
                 <img
                     src="/images/banner/banner.png"
                     alt=""
-                    className="w-full cursor-pointer"
+                    className="w-full h-50 sm:h-60 md:h-70 cursor-pointer bg-cover"
                 />
 
             </div>
