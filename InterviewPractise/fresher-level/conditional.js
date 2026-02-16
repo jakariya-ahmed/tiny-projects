616368a797a370a40e81a0def549bab10aa8c737
jek9356 <devjakariya9356@gmail.com>
2026-02-16 10:12:53 +0600

    conditional statements

---
+++ InterviewPractise/fresher-level/conditional.js
@@ -0,0 +1,44 @@
+/** Conditaonal statement
+ * if / else 
+ * switch
+ * Loops: for, while, do while
+ * break and continue
+ * 
+ */
+
+
+
+// if / else
+    let age = 18;
+    if (age >= 18) { 
+        console.log('You are an adult.');
+    }  else {
+        console.log('You are a minor.');
+    }
+
+// Multiple conditins with else if 
+    let score = 20;
+    if (score >= 90) {
+        console.log('Grade: A');
+    } else if (score >= 80) {
+        console.log('Grade: B');
+    } else {
+        console.log("Failed");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
