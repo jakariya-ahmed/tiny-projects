d2c64f5ba69307ed0fa660154c5ead2143d87dc2
jek9356 <devjakariya9356@gmail.com>
2026-02-16 11:37:07 +0600

    sovle a string into reserve string

--- InterviewPractise/fresher-level/conditional.js
+++ InterviewPractise/fresher-level/conditional.js
@@ -47,23 +47,52 @@ switch (day) {
 
 // For Loop: known number of iterations
 for (let i = 1; i <= 20; i++) {
-    console.log(i); // print 1 to 20 numbers
+    // console.log(i); // print 1 to 20 numbers
 }
 
 
 /** Five Basic Problems and Solve by for loop */
 // 1. Find the sum of first n natural numbers
-let n = 100;
 let sum = 0;
-for (let i = 1; i <= n; i++) {
+for (let i = 1; i <= 100; i++) {
     sum += i // sum = sum + i;
 }
 // console.log(`The sum of first ${n} natural numbers is: ${sum}`);
 
 
- // Multiplication Table of a number
+ // 2. Multiplication Table of a number
  let num = 5;
     for (let i = 1; i <= 10; i++) {
-        console.log(`${num} * ${i} = ${num * i}`);
+        // console.log(`${num} * ${i} = ${num * i}`);
     }
 
+
+// 3. Count even number
+let countEven = 0;
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        countEven++;
+        // console.log(i);
+    }
+}
+
+// console.log(`Total even numbers between 1 and 20: ${countEven}`);
+
+// 4. Count odd number
+let coundOdd = 0;
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 !== 0) {
+        coundOdd++;
+        // console.log(i);
+    }
+}
+
+
+// 5. Reverse a string
+let str = "Bangladesh";
+let reversedStr = "";
+for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i]; // reversedStr = reversedStr + str[i];
+}
+
+// console.log(`Reversed String: ${reversedStr}`);
